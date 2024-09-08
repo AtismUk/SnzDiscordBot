c811be4fa801d666dbe636fa2f4945ca14bb2d5c
Lev Kotovskiy <kotovskiy@studio-aw.ru>
2024-09-08 10:58:48 +0300

    service base

--- SnzDiscordBot/Program.cs
+++ SnzDiscordBot/Program.cs
@@ -1,65 +1,9 @@
-﻿using Discord;
-using Discord.Interactions;
-using Discord.WebSocket;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration.Json;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SnzDiscordBot;
 
-namespace SnzDiscordBot;
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
 
-class Program
-{
-    private static DiscordSocketClient _client;
-    private InteractionService _commands;
-    
-    static async Task Main(string[] args) => await new Program().RunAsync();
-
-    public async Task RunAsync()
-    {
-
-        // Получение всех сервисов по DI
-        var services = ConfigureServices();
-                
-        //Настройка Disocrd
-        _client = services.GetRequiredService<DiscordSocketClient>();
-        _commands = services.GetRequiredService<InteractionService>();
-        _client.Log += Log;
-        
-        _client.Ready += async () =>
-        {
-            await _commands.RegisterCommandsGloballyAsync();
-        };
-        await _client.LoginAsync(TokenType.Bot, services.GetRequiredService<IConfiguration>()["Discord:Token"]);
-        await _client.StartAsync();
-
-        await services.GetRequiredService<CommandHandler>().InitializeAsync();
-        await Task.Delay(-1);
-
-    }
-
-    public ServiceProvider ConfigureServices()
-    {
-        var config = new DiscordSocketConfig()
-        {
-            GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMembers
-        };
-
-        IConfiguration configuration = new ConfigurationBuilder()
-        .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName)
-        .AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true)
-        .Build();
-
-        return new ServiceCollection()
-            .AddSingleton<DiscordSocketClient>(x => new DiscordSocketClient(config))
-            .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
-            .AddSingleton<CommandHandler>()
-            .AddSingleton<IConfiguration>(configuration)
-            .BuildServiceProvider();
-    }
-
-    static Task Log(LogMessage message)
-    {
-        Console.WriteLine(message.ToString());
-        return Task.CompletedTask;
-    }
-}
+var host = builder.Build();
+host.Run();
---
+++ SnzDiscordBot/Worker.cs
@@ -0,0 +1,72 @@
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SnzDiscordBot;
+
+public class Worker : BackgroundService
+{
+    private static ILogger<Worker> _logger;
+    private static DiscordSocketClient _client;
+    private InteractionService _commands;
+        
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+    
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        // Получение всех сервисов по DI
+        var services = ConfigureServices();
+                
+        //Настройка Disocrd
+        _client = services.GetRequiredService<DiscordSocketClient>();
+        _commands = services.GetRequiredService<InteractionService>();
+        _client.Log += Log;
+        
+        _client.Ready += async () =>
+        {
+            await _commands.RegisterCommandsGloballyAsync();
+        };
+        await _client.LoginAsync(TokenType.Bot, services.GetRequiredService<IConfiguration>()["Discord:Token"]);
+        await _client.StartAsync();
+
+        await services.GetRequiredService<CommandHandler>().InitializeAsync();
+        
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+
+    public ServiceProvider ConfigureServices()
+    {
+        var config = new DiscordSocketConfig()
+        {
+            GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMembers
+        };
+
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName)
+            .AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        return new ServiceCollection()
+            .AddSingleton<DiscordSocketClient>(x => new DiscordSocketClient(config))
+            .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+            .AddSingleton<CommandHandler>()
+            .AddSingleton<IConfiguration>(configuration)
+            .BuildServiceProvider();
+    }
+
+    static Task Log(LogMessage message)
+    {
+        _logger.LogInformation($"[{message.Severity}] {message.Source}: {message.Message}");
+        return Task.CompletedTask;
+    }
+}

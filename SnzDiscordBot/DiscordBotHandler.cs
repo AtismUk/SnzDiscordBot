c339c81010659f5b4f55889ac326dbfaae383080
Anton <fangun133@gamil.com>
2024-09-11 00:21:53 +0900

    Hosting

---
+++ SnzDiscordBot/DiscordBotHandler.cs
@@ -0,0 +1,54 @@
+﻿using Discord.Interactions;
+using Discord.WebSocket;
+using Discord;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SnzDiscordBot
+{
+    internal class DiscordBotHandler
+    {
+        private readonly DiscordSocketClient _discordSocketClient;
+        private readonly InteractionService _interactionService;
+        private readonly IConfiguration _config;
+        private readonly CommandHandler _commandHandler;
+        public DiscordBotHandler(DiscordSocketClient client, InteractionService intService, IConfiguration config, CommandHandler commandHandler)
+        {
+            _discordSocketClient = client;
+            _interactionService = intService;
+            _config = config;
+            _commandHandler = commandHandler;
+        }
+
+        public async Task RunAsync()
+        {
+
+            //Настройка Disocrd
+            var _client = _discordSocketClient;
+            var _commands = _interactionService;
+
+            _client.Log += Log;
+            _client.Ready += async () =>
+            {
+                await _commands.RegisterCommandsGloballyAsync();
+            };
+            await _client.LoginAsync(TokenType.Bot, _config["Discord:Token"]);
+            await _client.StartAsync();
+
+            await _commandHandler.InitializeAsync();
+            await Task.Delay(-1);
+
+        }
+
+        static Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
--- SnzDiscordBot/Program.cs
+++ SnzDiscordBot/Program.cs
@@ -4,6 +4,7 @@ using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Hosting;
 
 namespace SnzDiscordBot;
 
@@ -12,50 +13,29 @@ class Program
     private static DiscordSocketClient _client;
     private InteractionService _commands;
     
-    static async Task Main(string[] args) => await new Program().RunAsync();
-
-    public async Task RunAsync()
+    static async Task Main(string[] args)
     {
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<DiscordSocketClient>(new DiscordSocketClient(new()
+                {
+                    GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMembers
+                }));
+                services.AddSingleton(x => new InteractionService(x.GetService<DiscordSocketClient>()));
+                services.AddSingleton<IConfiguration>(new ConfigurationBuilder()
+                        .AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true)
+                        .Build());
+                services.AddSingleton<CommandHandler>();
 
-        // Получение всех сервисов по DI
-        var services = ConfigureServices();
-                
-        //Настройка Disocrd
-        _client = services.GetRequiredService<DiscordSocketClient>();
-        _commands = services.GetRequiredService<InteractionService>();
-        _client.Log += Log;
-        
-        _client.Ready += async () =>
-        {
-            await _commands.RegisterCommandsGloballyAsync();
-        };
-        await _client.LoginAsync(TokenType.Bot, services.GetRequiredService<IConfiguration>()["Discord:Token"]);
-        await _client.StartAsync();
+                services.AddSingleton<DiscordBotHandler>();
+            }).Build();
 
-        await services.GetRequiredService<CommandHandler>().InitializeAsync();
-        await Task.Delay(-1);
+        var discordBot = host.Services.GetService<DiscordBotHandler>();
 
+        await discordBot!.RunAsync();
     }
 
-    public ServiceProvider ConfigureServices()
-    {
-        var config = new DiscordSocketConfig()
-        {
-            GatewayIntents = GatewayIntents.Guilds | GatewayIntents.GuildMembers
-        };
-
-        IConfiguration configuration = new ConfigurationBuilder()
-        .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName)
-        .AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true)
-        .Build();
-
-        return new ServiceCollection()
-            .AddSingleton<DiscordSocketClient>(x => new DiscordSocketClient(config))
-            .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
-            .AddSingleton<CommandHandler>()
-            .AddSingleton<IConfiguration>(configuration)
-            .BuildServiceProvider();
-    }
 
     static Task Log(LogMessage message)
     {

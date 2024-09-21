2ddb10252126efd7e3339105f9ef136391a3b08d
Lev Kotovskiy <kotovskiy@studio-aw.ru>
2024-09-21 15:05:15 +0300

    improve

--- SnzDiscordBot/Models/InteractionModels/MentionModel.cs
+++ SnzDiscordBot/Models/InteractionModels/MentionModel.cs
@@ -8,18 +8,18 @@ public class MentionModel : IModal
     public string Title => "Укажите данные для оповещения"; 
     
     [InputLabel("Заголовок")]
-    [ModalTextInput("title_input")]
+    [ModalTextInput("title_input", minLength: 5, maxLength: 100, placeholder: "Ваш заголовок.")]
     public string UserTitle { get; set; } = default!;
 
     [InputLabel("Информация")]
-    [ModalTextInput("description_input", style: TextInputStyle.Paragraph)]
+    [ModalTextInput("description_input", minLength: 20, style: TextInputStyle.Paragraph)]
     public string Description { get; set; } = default!;
     
     [InputLabel("Ссылка на превью")]
-    [ModalTextInput("preview_input")]
+    [ModalTextInput("preview_input", placeholder: "Ссылка на превью. Поставьте минус если без превью.")]
     public string ImageUrl { get; set; } = default!;
     
     [InputLabel("Ссылка на лого")]
-    [ModalTextInput("logo_input")]
+    [ModalTextInput("logo_input", placeholder: "Ссылка на лого. Поставьте минус если без лого.")]
     public string ThumbnailUrl { get; set; } = default!;
 }

b60c900c11dd82dfff8f119b24140f66ac1d51d0
Lev Kotovskiy <kotovskiy@studio-aw.ru>
2024-09-23 14:08:23 +0300

    уга буга

---
+++ SnzDiscordBot/DataBase/Entities/SettingsEntity.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SnzDiscordBot.DataBase.Entities;
+
+[Table("settings")]
+public class SettingsEntity : BaseEntity
+{
+    public ulong GuildId { get; set; }
+    public ulong AuditChannelId { get; set; } = 0;
+    public ulong ApplicationChannelId { get; set; } = 0;
+    public ulong ApplicationAddRoleId { get; set; } = 0;
+    public ulong ApplicationRemoveRoleId { get; set; } = 0;
+    public ulong EventChannelId { get; set; } = 0;
+    public ulong NewsChannelId { get; set; } = 0;
+    public ulong ScheduleChannelId { get; set; } = 0;
+}

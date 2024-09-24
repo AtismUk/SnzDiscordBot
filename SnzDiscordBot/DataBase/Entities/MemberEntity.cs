a9dc39fa3805a8ae2bcc0fe4daca404665a28526
Lev Kotovskiy <kotovskiy@studio-aw.ru>
2024-09-25 01:37:03 +0300

    member entity

---
+++ SnzDiscordBot/DataBase/Entities/MemberEntity.cs
@@ -0,0 +1,62 @@
+namespace SnzDiscordBot.DataBase.Entities;
+
+public class MemberEntity : BaseEntity
+{
+    public ulong GuildId { get; set; }
+    public ulong UserId { get; set; }
+    public string Username { get; set; }
+    public Rank Rank { get; set; } = Rank.Rookie;
+    public Group Group { get; set; } = Group.Unknown;
+    public Role[] Roles { get; set; } = [];
+    public Status Status { get; set; } = Status.Active;
+}
+
+public enum Rank : int
+{
+    Rookie,
+    Private,
+    Corporal,
+    JuniorSergeant,
+    Sergeant,
+    SeniorSergeant,
+    Ensign,
+    SeniorEnsign,
+    JuniorLieutenant,
+    Lieutenant,
+    SeniorLieutenant,
+    Captain,
+    Major,
+    LieutenantColonel,
+    Colonel
+}
+
+public enum Group : int
+{
+    Unknown,
+    First,
+    Second,
+    Third,
+    TransportSupport,
+    FireSupport,
+    Recon,
+    Leadership
+}
+
+public enum Role : int
+{
+    Commander,
+    DeputyCommander,
+    HumanResources,
+    Instructor,
+    Creative,
+    Technical,
+    Founder
+}
+
+public enum Status : int
+{
+    Unknown,
+    Active,
+    Vacation,
+    Reserve
+}

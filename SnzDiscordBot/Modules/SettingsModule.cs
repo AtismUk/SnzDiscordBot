442378d40b6b90b9fddf9b975fe5f6600281f201
Lev Kotovskiy <kotovskiy@studio-aw.ru>
2024-09-25 02:18:57 +0300

    wtf

--- SnzDiscordBot/Modules/SettingsModule.cs
+++ SnzDiscordBot/Modules/SettingsModule.cs
@@ -18,12 +18,6 @@ public class SettingsModule : InteractionModuleBase<SocketInteractionContext>
     [RequireUserPermission(GuildPermission.ManageChannels)]
     public async Task ConfigApplicationCommand(IChannel? audit_channel = null, IChannel? application_channel = null, IRole? application_remove_role = null, IRole? application_add_role = null, IChannel? event_channel = null, IChannel? news_channel = null, IChannel? schedule_channel = null)
     {
-        if (Context.User is not SocketGuildUser userGuild)
-            return;
-        
-        if (!userGuild.Roles.Any(x => x.Permissions.BanMembers))
-            return;
-        
         await _settingsService.SaveSettingsAsync(Context.Guild.Id, audit_channel?.Id, application_channel?.Id, application_remove_role?.Id, application_add_role?.Id, event_channel?.Id, news_channel?.Id, schedule_channel?.Id);
 
         await RespondAsync("Данные успешно сохранены!", ephemeral: true);

69dd8b8ed46e999984104803bc3e5b48ef7708a3
Lev Kotovskiy <kotovskiy@studio-aw.ru>
2024-09-25 01:14:48 +0300

    forgot

--- SnzDiscordBot/DataBase/AppDbContext.cs
+++ SnzDiscordBot/DataBase/AppDbContext.cs
@@ -1,20 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SnzDiscordBot.DataBase.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SnzDiscordBot.DataBase
+namespace SnzDiscordBot.DataBase;
+
+public sealed class AppDbContext : DbContext
 {
-    public class AppDbContext : DbContext
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-            Database.EnsureCreated();
-        }
-
-        public DbSet<SettingsEntity> SettingsEntities { get; set; }
+        Database.EnsureCreated();
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<SettingsEntity>()
+            .HasIndex(x => x.GuildId)
+            .IsUnique();
+        
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<SettingsEntity> SettingsEntities { get; set; }
 }
--- SnzDiscordBot/DataBase/Entities/BaseEntity.cs
+++ SnzDiscordBot/DataBase/Entities/BaseEntity.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SnzDiscordBot.DataBase.Entities;
 
-namespace SnzDiscordBot.DataBase.Entities
+public class BaseEntity
 {
-    public class BaseEntity
-    {
-        public int Id { get; set; }
-    }
+    public int Id { get; set; } = 0;
 }

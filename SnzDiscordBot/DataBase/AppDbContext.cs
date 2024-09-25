44b1dca5dc5e28e5082327dbea8bfaeb8435f88a
Lev Kotovskiy <kotovskiy@studio-aw.ru>
2024-09-25 04:27:44 +0300

    remove from branch shit

--- SnzDiscordBot/DataBase/AppDbContext.cs
+++ SnzDiscordBot/DataBase/AppDbContext.cs
@@ -7,7 +7,6 @@ public sealed class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        Database.EnsureDeleted();
         Database.EnsureCreated();
     }
 

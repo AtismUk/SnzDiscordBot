9aab5e874257a410e4fd7fc267a8546b720186b1
Lev Kotovskiy <kotovskiy@studio-aw.ru>
2024-09-26 21:50:37 +0300

    Application update

--- SnzDiscordBot/Modules/ApplicationModule.cs
+++ SnzDiscordBot/Modules/ApplicationModule.cs
@@ -1,59 +1,61 @@
 using System.Text.RegularExpressions;
 using Discord;
 using Discord.Interactions;
+using SnzDiscordBot.DataBase.Entities;
 using SnzDiscordBot.Models.InteractionModels;
 using SnzDiscordBot.Services.Interfaces;
-using Color = Discord.Color;
+using Group = SnzDiscordBot.DataBase.Entities.Group;
 
 namespace SnzDiscordBot.Modules;
 
 public class ApplicationModule : InteractionModuleBase<SocketInteractionContext>
 {
-    #region Buttons
-
-    private readonly ButtonBuilder _acceptButton = new() {
-        CustomId = "accept_button", 
-        Label = "Принять", 
-        Style = ButtonStyle.Success,
-    };
-    private readonly ButtonBuilder _cancelButton = new() {
-        CustomId = "cancel_button",
-        Label = "Отклонить",
-        Style = ButtonStyle.Secondary,
-    };
-
-
-    #endregion
     private readonly ISettingsService _settingsService;
+    private readonly IMemberService _memberService;
     
-    public ApplicationModule(ISettingsService settings)
+    public ApplicationModule(ISettingsService settings, IMemberService memberService)
     {
         _settingsService = settings;
+        _memberService = memberService;
     }
     
     [SlashCommand("application", "Отправить заявку на вступление")]
     public async Task ApplicationCommand()
     {
         var settings = await _settingsService.GetSettingsAsync(Context.Guild.Id);
+        if (settings == null)
+        {
+            await RespondAsync("Ошибка базы данных! Не удалось получить настройки.", ephemeral: true);
+            return;
+        }
         
-        var channelId = settings.ApplicationChannelId;
-
-        if (Context.Guild.Channels.FirstOrDefault(x => x.Id == channelId) != null)
+        if (Context.Channel.Id == settings.ApplicationChannelId)
         {
             await RespondWithModalAsync<ApplicationModel>("application_form");
+        }
+        else
+        {
+            await RespondAsync("В этом канале запрещено подавать заявку!", ephemeral: true);
         }
     }
 
     [ModalInteraction("application_form")]
     public async Task HandlerApplicationForm(ApplicationModel form)
     {
+        var settings = await _settingsService.GetSettingsAsync(Context.Guild.Id);
+        if (settings == null)
+        {
+            await RespondAsync("Ошибка базы данных! Не удалось получить настройки.", ephemeral: true);
+            return;
+        }
+        
         if (!Regex.IsMatch(form.Nick, @"^[a-zA-Z0-9]+$")) {
               await RespondAsync("Ваш позывной может содержать только латинские символы и цифры!", ephemeral: true);  
         }
         
-        if (!byte.TryParse(form.Points, out var point) || point < 1 || point > 10)
+        if (!byte.TryParse(form.Points, out var point) || point > 10)
         {
-            await RespondAsync("Ваша субъективная оценка игры должны быть в диапазоне от 1 по 10!", ephemeral: true);
+            await RespondAsync("Оценка вашей игры должны быть в диапазоне от 0 до 10!", ephemeral: true);
         }
 
         if (!long.TryParse(form.SteamId, out var steamId) || form.SteamId.Length != 17)
@@ -78,14 +80,25 @@ public class ApplicationModule : InteractionModuleBase<SocketInteractionContext>
         embed.AddField("Позывной", form.Nick);
         embed.AddField("Steam", "https://steamcommunity.com/profiles/" + steamId.ToString());
         embed.AddField("От куда узнал", form.Info);
-        embed.AddField("Субъективная оценка игры", point.ToString() + "/10");
+        embed.AddField("Субъективность оценка игры", point.ToString() + "/10");
 
         ComponentBuilder componentBuilder = new();
-        componentBuilder.WithButton(_acceptButton);
-        componentBuilder.WithButton(_cancelButton);
+        
+        ButtonBuilder acceptButton = new() {
+            CustomId = "accept_button", 
+            Label = "Принять", 
+            Style = ButtonStyle.Success,
+        };
+        ButtonBuilder cancelButton = new() {
+            CustomId = "cancel_button",
+            Label = "Отклонить",
+            Style = ButtonStyle.Secondary,
+        };
+        
+        componentBuilder.WithButton(acceptButton);
+        componentBuilder.WithButton(cancelButton);
 
         await RespondAsync(embed: embed.Build(), components: componentBuilder.Build());
-
     }
 
 
@@ -93,37 +106,62 @@ public class ApplicationModule : InteractionModuleBase<SocketInteractionContext>
     [RequireUserPermission(GuildPermission.ManageRoles)]
     public async Task AcceptButtonHandler()
     {
+        #region Проверки
 
         var settings = await _settingsService.GetSettingsAsync(Context.Guild.Id);
-        
-        #region Изменяем пользователя
-
-        var interaction = (IComponentInteraction)Context.Interaction;
-
-        var message = interaction.Message;
-
-        var embedProper = message.Embeds.First();
-
-        await Context.Guild.DownloadUsersAsync();
-
-        var userId = ulong.Parse(embedProper.Footer!.Value.Text);
-
-        var user = Context.Guild.Users.FirstOrDefault(x => x.Id == userId);
-
-        if (user == null)
+        if (settings == null)
         {
-            await RespondAsync("Пользователь не найден на сервере", ephemeral: true);
+            await RespondAsync("Ошибка базы данных! Не удалось получить настройки.", ephemeral: true);
+            return;
+        }
+        
+        var interaction = (IComponentInteraction)Context.Interaction;
+        if (interaction == null)
+        {
+            await RespondAsync("Ошибка! Не удалось найти интеракцию.", ephemeral: true);
+            return;
+        }
+        
+        var message = interaction.Message;
+        if (message == null)
+        {
+            await RespondAsync("Ошибка! Не удалось найти сообщение заявки.", ephemeral: true);
+            return;
+        }
+        
+        var embedProper = message.Embeds.First();
+        if (embedProper == null)
+        {
+            await RespondAsync("Ошибка! Не удалось найти Embed из старого сообщения.", ephemeral: true);
+            return;
         }
 
-        await user!.RemoveRoleAsync(settings.ApplicationRemoveRoleId);
+        if (!ulong.TryParse(embedProper.Footer!.Value.Text, out var userId))
+        {
+            await RespondAsync("Ошибка! Не удалось преобразовать ID пользователя.", ephemeral: true);
+            return;
+        }
+        
+        var user = Context.Guild.Users.FirstOrDefault(x => x.Id == userId);
+        if (user == null)
+        {
+            await RespondAsync("Ошибка! Пользователь не найден на сервере. Заявка будет удалена.", ephemeral: true);
+            await interaction.Message.DeleteAsync();
+            return;
+        }
 
+        #endregion
+        
+        #region Изменяем пользователя
+        
+        await user.RemoveRoleAsync(settings.ApplicationRemoveRoleId);
         await user.AddRoleAsync(settings.ApplicationAddRoleId);
-
         await user.ModifyAsync(x => x.Nickname = "[SNZ] " + embedProper.Fields[0].Value);
-
 
         #endregion
 
+        var member = await _memberService.UpdateMemberAsync(Context.Guild.Id, user.Id, embedProper.Fields[0].Value, Rank.Rookie, Group.Unknown, [], Status.Active);
+        
         #region Изменение Embed
 
         var embed = new EmbedBuilder()
@@ -142,18 +180,22 @@ public class ApplicationModule : InteractionModuleBase<SocketInteractionContext>
         embed.AddField("Субъективная оценка игры", embedProper.Fields[3].Value);
 
         ComponentBuilder componentBuilder = new();
-
-        var acceptButtonDisabled = _acceptButton;
-        acceptButtonDisabled.IsDisabled = true;
         
+        ButtonBuilder acceptButton = new() {
+            CustomId = "accept_button", 
+            Label = "Принять", 
+            Style = ButtonStyle.Success,
+            IsDisabled = true
+        };
+        ButtonBuilder cancelButton = new() {
+            CustomId = "cancel_button",
+            Label = "Отклонить",
+            Style = ButtonStyle.Secondary,
+            IsDisabled = true
+        };
 
-        var cancelButtonDisabled = _cancelButton;
-        cancelButtonDisabled.IsDisabled = true;
-        cancelButtonDisabled.Style = ButtonStyle.Secondary;
-
-        componentBuilder.WithButton(acceptButtonDisabled);
-        componentBuilder.WithButton(cancelButtonDisabled);
-
+        componentBuilder.WithButton(acceptButton);
+        componentBuilder.WithButton(cancelButton);
 
         await message.ModifyAsync(x =>
         {
@@ -162,9 +204,15 @@ public class ApplicationModule : InteractionModuleBase<SocketInteractionContext>
             
         });
 
-        await RespondAsync("Заявка принята", ephemeral: true);
         #endregion
 
+        if (member != null)
+        {
+            await RespondAsync("Заявка принята успешно!", ephemeral: true);
+            return;
+        }
+        
+        await RespondAsync("Заявка принята с ошибкой базы данных.", ephemeral: true);
     }
 
 
@@ -172,28 +220,62 @@ public class ApplicationModule : InteractionModuleBase<SocketInteractionContext>
     [RequireUserPermission(GuildPermission.ManageRoles)]
     public async Task CancelButtonHandler()
     {
-        await RespondWithModalAsync<CancelModel>("cancel_form");
-
+        await RespondWithModalAsync<ApplicationCancelModel>("cancel_form");
     }
 
 
     [ModalInteraction("cancel_form")]
-    public async Task HandlerCancelForm(CancelModel form)
+    public async Task HandlerCancelForm(ApplicationCancelModel form)
     {
+        #region Проверки
 
-        var interaction = (IModalInteraction)Context.Interaction;
+        var interaction = (IComponentInteraction)Context.Interaction;
+        if (interaction == null)
+        {
+            await RespondAsync("Ошибка! Не удалось найти интеракцию.", ephemeral: true);
+            return;
+        }
 
         var message = interaction.Message;
+        if (message == null)
+        {
+            await RespondAsync("Ошибка! Не удалось найти сообщение заявки.", ephemeral: true);
+            return;
+        }
 
         var embedProper = message.Embeds.First();
+        if (embedProper == null)
+        {
+            await RespondAsync("Ошибка! Не удалось найти Embed из старого сообщения.", ephemeral: true);
+            return;
+        }
 
-        var userId = ulong.Parse(embedProper.Footer!.Value.Text);
+        if (!ulong.TryParse(embedProper.Footer!.Value.Text, out var userId))
+        {
+            await RespondAsync("Ошибка! Не удалось преобразовать ID пользователя.", ephemeral: true);
+            return;
+        }
 
         var user = Context.Guild.Users.FirstOrDefault(x => x.Id == userId);
-
         if (user == null)
         {
-            await RespondAsync("Пользователь не найден на сервере", ephemeral: true);
+            await RespondAsync("Ошибка! Пользователь не найден на сервере. Заявка будет удалена.", ephemeral: true);
+            await interaction.Message.DeleteAsync();
+            return;
+        }
+
+        #endregion
+        
+        switch (form.Kick.ToLower())
+        {
+            case "да":
+                await user.KickAsync("Заявка отклонена с исключением пользователя.");
+                break;
+            case "нет":
+                break;
+            default:
+                await RespondAsync("Ошибка! Не удалось определить будет пользователь исключен или нет.", ephemeral: true);
+                return;
         }
 
         #region Изменение Embed
@@ -212,20 +294,26 @@ public class ApplicationModule : InteractionModuleBase<SocketInteractionContext>
         embed.AddField("Steam", embedProper.Fields[1].Value);
         embed.AddField("От куда узнал", embedProper.Fields[2].Value);
         embed.AddField("Субъективная оценка игры", embedProper.Fields[3].Value);
-        embed.AddField("Причина отклонения", form.Text);
+        embed.AddField("Причина отклонения", form.Reason);
+        embed.AddField("Пользователь исключен?", form.Kick);
 
         ComponentBuilder componentBuilder = new();
 
-
-        var acceptButtonDisabled = _acceptButton;
-        acceptButtonDisabled.IsDisabled = true;
-        acceptButtonDisabled.Style = ButtonStyle.Secondary;
-
-        var cancelButtonDisabled = _cancelButton;
-        cancelButtonDisabled.IsDisabled = true;
+        ButtonBuilder acceptButton = new() {
+            CustomId = "accept_button", 
+            Label = "Принять", 
+            Style = ButtonStyle.Success,
+            IsDisabled = true
+        };
+        ButtonBuilder cancelButton = new() {
+            CustomId = "cancel_button",
+            Label = "Отклонить",
+            Style = ButtonStyle.Secondary,
+            IsDisabled = true
+        };
         
-        componentBuilder.WithButton(acceptButtonDisabled);
-        componentBuilder.WithButton(cancelButtonDisabled);
+        componentBuilder.WithButton(acceptButton);
+        componentBuilder.WithButton(cancelButton);
 
 
         await message.ModifyAsync(x =>
@@ -233,8 +321,9 @@ public class ApplicationModule : InteractionModuleBase<SocketInteractionContext>
             x.Embed = embed.Build();
             x.Components = componentBuilder.Build();
         });
-
-        await RespondAsync("Заявка отклонена", ephemeral: true);
+        
         #endregion
+        
+        await RespondAsync("Заявка отклонена успешно!", ephemeral: true);
     }
 }
